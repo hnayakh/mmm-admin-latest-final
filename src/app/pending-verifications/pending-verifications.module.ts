31bd1f083703e38b72ff85f7c68eaebe2582d459
Bibhudutta <bibhudutta.das44@gmail.com>
2022-09-21 18:00:36 +0530

    changed

--- src/app/pending-verifications/pending-verifications.module.ts
+++ src/app/pending-verifications/pending-verifications.module.ts
@@ -4,16 +4,25 @@ import { PendingVerificationsRoutingModule } from "./pending-verifications-routi
 import { VerificationListComponent } from "./verification-list/verification-list.component";
 import { ManageVerificationComponent } from "./manage-verification/manage-verification.component";
 import { DataTablesModule } from "angular-datatables";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CarouselModule } from "ngx-owl-carousel-o";
 import { MatTableModule } from "@angular/material/table";
-import { MatSliderModule } from '@angular/material/slider';
-import { NgxMuiDatatablesModule } from 'ngx-mui-datatables';
+import { MatSliderModule } from "@angular/material/slider";
+import { NgxMuiDatatablesModule } from "ngx-mui-datatables";
 
 console.log("active");
 
 @NgModule({
-  declarations: [VerificationListComponent,ManageVerificationComponent],
-  imports: [CommonModule, PendingVerificationsRoutingModule,DataTablesModule,NgxMuiDatatablesModule,BrowserAnimationsModule,MatTableModule,MatSliderModule,CarouselModule],
+  declarations: [VerificationListComponent, ManageVerificationComponent],
+  imports: [
+    CommonModule,
+    PendingVerificationsRoutingModule,
+    DataTablesModule,
+    NgxMuiDatatablesModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSliderModule,
+    CarouselModule,
+  ],
 })
 export class PendingVerificationsModule {}
