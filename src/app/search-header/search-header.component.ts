c5dafa6057c55f72fb580685e08b3c223a61dc4f
shashanka panda <shashanka.panda@gmail.com>
2023-11-05 02:01:09 +0530

    Search functionality implemented

--- src/app/search-header/search-header.component.ts
+++ src/app/search-header/search-header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+//import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-search-header',
@@ -8,12 +9,33 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output
 })
 export class SearchHeaderComponent implements OnInit {
   @Input()
-   items :[];
+   items :any[]=[];
+   @Output() onMMIDSelected = new EventEmitter();
+   //announcer = inject(LiveAnnouncer);
  
   constructor() { }
 
   ngOnInit() {
     console.log("items",this.items);
+
+    
   }
+  onChipClick(mmId:string){
+    this.onMMIDSelected.emit({mmid:mmId})
+
+  }
+
+  // removeKeyword(keyword:any) {
+    
+  //   const index = this.items.indexOf(keyword);
+  //   if (index >= 0) {
+  //     this.items.splice(index, 1);
+
+  //     //this.announcer.announce(`removed ${keyword}`);
+  //   }
+  // }
+ 
+
+  
 
 }
