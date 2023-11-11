981d71cba2bbc723e7a851cb05210879885f3c43
Shashanka <shashanka.panda@gmail.com>
2023-11-11 19:16:22 +0530

    admin latest changes

--- src/app/profile-verification/profile-verification.component.ts
+++ src/app/profile-verification/profile-verification.component.ts
@@ -10,8 +10,14 @@ export class ProfileVerificationComponent implements OnInit {
 
   constructor(private userService: UserService) { }
   documents:any[];
+  userDetails:any;
   ngOnInit() {
-   // this.userService.getProfileDocuments();
+    this.userService.getProfileDocuments("c0504e22-8819-44ce-aa37-c084fecb04ea").subscribe((result:any)=>{
+      if(result.data)
+      this.documents=result.data.imageDetails.map((doc:any)=>{return {docUrl:doc.imageURL, docType:doc.idProof}})
+      this.userDetails=result.data.userAbout;
+      console.log("this.documents",this.documents)
+    })
 
   }
 
